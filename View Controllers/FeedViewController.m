8174279b3b33e49e1cc0c625af901b1ef7e1235f
Briana Mason <brm14@fb.com>
2020-07-08 15:06:52 -0700

    User can see most recent 20 photos that were posted.

--- View Controllers/FeedViewController.m
+++ View Controllers/FeedViewController.m
@@ -10,9 +10,14 @@
 #import <Parse/Parse.h>
 #import <UIKit/UIKit.h>
 #import "SceneDelegate.h"
+#import "Post.h"
+#import "PostTableViewCell.h"
 #import "LoginViewController.h"
 
-@interface FeedViewController ()
+@interface FeedViewController () <UITableViewDelegate, UITableViewDataSource>
+
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
+
 
 @end
 
@@ -20,6 +25,14 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    
+    [self fetchFeed];
+    
+    self.tableView.rowHeight = UITableViewAutomaticDimension;
+
     // Do any additional setup after loading the view.
     
     
@@ -39,9 +52,32 @@
     
 }
 
+- (void)fetchFeed {
+    
+    //set up the query
+    PFQuery *postQuery = [Post query];
+    [postQuery orderByDescending:@"createdAt"];
+    [postQuery includeKey:@"author"];
+    postQuery.limit = 20;
+    
+    //asynchronously get data from database
+    [postQuery findObjectsInBackgroundWithBlock:^(NSArray <Post *>* _Nullable posts, NSError * _Nullable error) {
+        if(posts)
+        {
+            self.feedPostsArray = (NSMutableArray *)posts;
+            //do something == load an array with posts
+            [self.tableView reloadData];
 
-
-
+            NSLog(@"hi");
+        }
+        else
+        {
+            NSLog(@"error: %@", error.localizedDescription);
+        }
+    }];
+    
+    
+}
 
 
 /*
@@ -53,5 +89,25 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+
+//necessary for UITableViewSource implementation: gets # of rows
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.feedPostsArray.count;
+}
+//necessary for UITableViewSource implementation: asks data source for a cell to insert
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+
+    //use the Post cell that's set up on the storyboard
+    PostTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"PostCell"];
+    //NSLog(@"cell: %@", cell);
+    
+    Post *post = self.feedPostsArray[indexPath.row];
+
+    [cell setPost:post];
+        
+    return cell;
+}
 
 @end
